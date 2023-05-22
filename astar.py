22e98f25d5951c5adaaf9d213f209abe12519682
Ankur-Deka <ankurnet1996@gmail.com>
2023-05-21 17:08:39 -0700

    visualizer

--- astar.py
+++ astar.py
@@ -1,20 +1,24 @@
+from copy import deepcopy
 import numpy as np
 import matplotlib.pyplot as plt
 from queue import PriorityQueue
 
-grid = np.array([[0,0,0,1,0],
-                 [0,1,0,1,0],
-                 [0,1,0,1,0],
-                 [0,1,0,0,0]])
+from utils import get_dist, get_grid, viz_grid, cell_type_codes
 
+grid = get_grid()
+speed = 1000
 H, W = grid.shape
-def get_dist(a1, a2):
-    return np.linalg.norm(np.array(a1)-np.array(a2))
 
-start = [3, 0]
-goal = [0, 4]
+start = [H-1, 0]
+goal = [0, W-1]
+grid_state = deepcopy(grid)
+grid_state[start[0], start[1]] = cell_type_codes["start"]
+grid_state[goal[0], goal[1]] = cell_type_codes["goal"]
+viz_grid(grid_state)
+# plt.pause(1/speed)                                                                                                   )
+
 frontier = PriorityQueue()
-frontier.put((-get_dist(start,goal), start))
+frontier.put((get_dist(start,goal), start))
 
 node_to_parent = {tuple(start): None}
 found = False
@@ -25,29 +29,48 @@ while not found and frontier.qsize():
         found = True
         break
     i,j = node
-    if i-1>=0 and grid[i,j]!=1 and (i-1,j) not in node_to_parent:
-        frontier.put((-get_dist([i-1,j], goal), [i-1,j]))
+    if i-1>=0 and grid[i-1,j]!=1 and (i-1,j) not in node_to_parent:
+        frontier.put((get_dist([i-1,j], goal), [i-1,j]))
+        grid_state[i-1, j] = cell_type_codes["exploring"]
         node_to_parent[(i-1,j)] = [i,j]
-    if i+1<H and grid[i,j]!=1 and (i+1,j) not in node_to_parent:
-        frontier.put((-get_dist([i+1,j], goal), [i+1,j]))
+    if i+1<H and grid[i+1,j]!=1 and (i+1,j) not in node_to_parent:
+        frontier.put((get_dist([i+1,j], goal), [i+1,j]))
+        grid_state[i+1, j] = cell_type_codes["exploring"]
         node_to_parent[(i+1,j)] = [i,j]
-    if j-1>=0 and grid[i,j]!=1 and (i,j-1) not in node_to_parent:
-        frontier.put((-get_dist([i,j-1], goal), [i,j-1]))
+    if j-1>=0 and grid[i,j-1]!=1 and (i,j-1) not in node_to_parent:
+        frontier.put((get_dist([i,j-1], goal), [i,j-1]))
+        grid_state[i, j-1] = cell_type_codes["exploring"]
         node_to_parent[(i,j-1)] = [i,j]
-    if j+1<W and grid[i,j]!=1 and (i,j+1) not in node_to_parent:
-        frontier.put((-get_dist([i,j+1], goal), [i,j+1]))
+    if j+1<W and grid[i,j+1]!=1 and (i,j+1) not in node_to_parent:
+        frontier.put((get_dist([i,j+1], goal), [i,j+1]))
+        grid_state[i, j+1] = cell_type_codes["exploring"]
         node_to_parent[(i,j+1)] = [i,j]
 
-path = [goal]
-node = goal
-while node!=start:
-    node = node_to_parent[tuple(node)]
-    path.append(node)
+    # viz
+    grid_state[node[0], node[1]] = cell_type_codes["done_exploring"]
+    grid_state[start[0], start[1]] = cell_type_codes["start"]
+    grid_state[goal[0], goal[1]] = cell_type_codes["goal"]
+    viz_grid(grid_state)
+    plt.pause(1/speed)
+plt.clf()
 
-ans = np.zeros((H,W))
-for node in path:
-    ans[node[0], node[1]] = 1
-print(ans)
+if found:
+    path = [goal]
+    node = goal
+    while node!=start:
+        node = node_to_parent[tuple(node)]
+        path.append(node)
+
+    ans = np.zeros((H,W))
+    for node in path:
+        ans[node[0], node[1]] = 1
+        grid_state[node[0], node[1]] = cell_type_codes["path"]
+    grid_state[start[0], start[1]] = cell_type_codes["start"]
+    grid_state[goal[0], goal[1]] = cell_type_codes["goal"]
+    print(ans)
+    plt.close()
+    viz_grid(grid_state)
+    plt.show()
 
 
 
---
+++ utils.py
@@ -0,0 +1,62 @@
+from collections import OrderedDict
+import numpy as np
+from matplotlib import colors
+import matplotlib.pyplot as plt
+
+
+def get_grid():
+    return np.array([[0,0,1,0,0,0,0,0,0,0,0,0,1,0,0],
+                     [0,0,1,0,0,1,0,0,0,0,0,0,1,0,0],
+                     [0,0,1,0,0,1,0,0,0,0,0,0,1,0,0],
+                     [0,0,1,0,0,1,1,1,1,1,1,1,1,0,0],
+                     [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0],
+                     [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0],
+                     [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0],
+                     [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0],
+                     [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0],
+                     [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0],
+                     [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0],
+                     [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0],
+                     [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0],
+                     [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0],
+                     [1,1,1,0,0,1,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
+
+
+# 0: traversible, white
+# 1: blocked, black
+# 2: exploring, cyan translucent
+# 3: done exploring, cyan
+# 4: start, red
+# 5: goal, green
+# 6: path, blue
+grid_colors = [colors.to_rgba("white"),
+               colors.to_rgba("black"),
+               colors.to_rgba("cyan") - np.array([0,0,0,0.8]),
+               colors.to_rgba("cyan"),
+               colors.to_rgba("red"),
+               colors.to_rgba("green"),
+               colors.to_rgba("blue")]
+cell_type_codes = dict([("traversible", 0),
+                  ("blocked", 1),
+                  ("exploring", 2),
+                  ("done_exploring", 3),
+                  ("start", 4),
+                  ("goal", 5),
+                  ("path", 6),])
+grid_cmap = colors.ListedColormap(grid_colors)
+
+def viz_grid(grid_state):
+    plt.clf()
+    plt.imshow(grid_state, cmap=grid_cmap, vmin = 0, vmax = len(grid_colors))
+
+
+if __name__ == "__main__":
+    grid = get_grid()
+    viz_grid(grid)
+    plt.show()
+
+
+def get_dist(a1, a2):
+    return np.linalg.norm(np.array(a1)-np.array(a2))
